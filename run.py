3d13564a213d4d19b0bc1e08c2f12435d13a26aa
Mark Wilde <markwilde29@gmail.com>
2018-07-06 13:00:07 +0000

    Python3 and Flask installed.Main py file created and 3 html files created. base.html, home.html and about.html. program is running correctly and is error free

--- run.py
+++ run.py
@@ -1,17 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Sparky fucking Wilde',
+        'title': 'Blog Post 1',
+        'content': '1st post content',
+        'posted_on': '06 July, 2018'
+    },
+    {
+        'author': 'Robert Albert "bert bert" Leyden',
+        'title': 'Blog Post 2',
+        'content': '2nd post content',
+        'posted_on': '07 July, 2018'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Home Page"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "About Page"
+    return render_template('about.html', title='Shit Yeah')
     
     
 if __name__ == '__main__':
